0d1ea40c8ab2b7871509957b5e952fa27dedb1b4
kotableOrKisaki <super_misha05@mail.ru>
2022-08-27 23:27:03 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+
+            while (true)
+            {
+                Console.WriteLine("На выход!");
+                answer = Console.ReadLine();
+                if (answer == "exit")
+                {
+                    Console.WriteLine("быбы");
+                    break;
+                }
+            }  
+        }
+    }
+}
